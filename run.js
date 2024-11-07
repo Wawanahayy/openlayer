09667446659688745bb361774d63ccc09ff12c3c
timplexz <98287702+Wawanahayy@users.noreply.github.com>
2024-11-07 21:13:02 +0700

    Update run.js

--- run.js
+++ run.js
@@ -152,7 +152,7 @@ function displayAccountData() {
 
     console.clear();
     console.log(chalk.green('----------------------------------------------------------------------------------------------------------------------------'));
-    console.log(chalk.green('ACCOUNT    |    ADDRESS     |       DATE/JAM (GMT+7):      | Total Poin:  |  Proxy:     | TIME RUN:    | JOIN MY CHANNEL TG:'));
+    console.log(chalk.green('ACCOUNT      |    ADDRESS     |    DATE/JAM (GMT+7):    | Total Poin:  |  Proxy:     | TIME RUN:    | JOIN MY CHANNEL TG:  |'));
     console.log(chalk.green('----------------------------------------------------------------------------------------------------------------------------'));
 
     accountsData.forEach(account => {
@@ -168,7 +168,7 @@ function displayAccountData() {
       );
     });
 
-    console.log(chalk.green('----------------------------------------------------------------------------------------------------------------------------'));
+    console.log(chalk.green('---------------------------------------------------------------------------------------------------------------------------|'));
   }, 1000);
 }
 
